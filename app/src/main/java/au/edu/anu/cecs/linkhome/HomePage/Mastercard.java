b9b9e01f0fb1bb6c6e3e4aff8d837f1c18f7ff4e
Nihar Jagdish Meshram <u7395484@anu.edu.au>
2022-10-18 17:07:50 +1100

    Updated Payment.

--- app/src/main/java/au/edu/anu/cecs/linkhome/HomePage/Mastercard.java
+++ app/src/main/java/au/edu/anu/cecs/linkhome/HomePage/Mastercard.java
@@ -2,19 +2,59 @@ package au.edu.anu.cecs.linkhome.HomePage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 import au.edu.anu.cecs.linkhome.Facade.PaymentMaker;
 import au.edu.anu.cecs.linkhome.R;
 
 public class Mastercard extends AppCompatActivity {
+    private EditText dateEdt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mastercard);
         Intent intent = getIntent();
+        //
+        dateEdt = findViewById(R.id.paymentDates);
+        // on below line we are adding click listener
+        // for our pick date button
+        dateEdt.setOnClickListener(v -> {
+            // on below line we are getting
+            // the instance of our calendar.
+            final Calendar c = Calendar.getInstance();
+
+            // on below line we are getting
+            // our day, month and year.
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // on below line we are creating a variable for date picker dialog.
+            DatePickerDialog datePickerDialog = new DatePickerDialog(
+                    // on below line we are passing context.
+                    Mastercard.this,
+                    (view, year1, monthOfYear, dayOfMonth) -> {
+                        // on below line we are setting date to our edit text.
+                        String text = (monthOfYear + 1) + "/" + year1;
+                        dateEdt.setText(text);
+                        System.out.println("here!");
+                    },
+                    // on below line we are passing year,
+                    // month and day for selected date in our date picker.
+                    year, month, day);
+            // at last we are calling show to
+            // display our date picker dialog.
+            datePickerDialog.show();
+
+
+        });
+        //
         String payMethod = intent.getStringExtra("payMasterCard");
         PaymentMaker paymentMaker = new PaymentMaker();
         if (payMethod.equals("mastercard")) paymentMaker.pay_mastercard();
--- app/src/main/java/au/edu/anu/cecs/linkhome/HomePage/Paypal.java
+++ app/src/main/java/au/edu/anu/cecs/linkhome/HomePage/Paypal.java
@@ -13,7 +13,7 @@ import au.edu.anu.cecs.linkhome.Facade.PaymentMaker;
 import au.edu.anu.cecs.linkhome.R;
 
 public class Paypal extends AppCompatActivity {
-
+    private EditText dateEdt;
     //private EditText dateEdt;
 
     @Override
@@ -21,6 +21,41 @@ public class Paypal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paypal);
         Intent intent = getIntent();
+        //
+        dateEdt = findViewById(R.id.paymentDates);
+        // on below line we are adding click listener
+        // for our pick date button
+        dateEdt.setOnClickListener(v -> {
+            // on below line we are getting
+            // the instance of our calendar.
+            final Calendar c = Calendar.getInstance();
+
+            // on below line we are getting
+            // our day, month and year.
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // on below line we are creating a variable for date picker dialog.
+            DatePickerDialog datePickerDialog = new DatePickerDialog(
+                    // on below line we are passing context.
+                    Paypal.this,
+                    (view, year1, monthOfYear, dayOfMonth) -> {
+                        // on below line we are setting date to our edit text.
+                        String text = (monthOfYear + 1) + "/" + year1;
+                        dateEdt.setText(text);
+                        System.out.println("here!");
+                    },
+                    // on below line we are passing year,
+                    // month and day for selected date in our date picker.
+                    year, month, day);
+            // at last we are calling show to
+            // display our date picker dialog.
+            datePickerDialog.show();
+
+
+        });
+        //
         String payMethod = intent.getStringExtra("payPaypal");
         PaymentMaker paymentMaker = new PaymentMaker();
         if (payMethod.equals("payPal")) paymentMaker.pay_paypal();
